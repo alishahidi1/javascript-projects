bb0ffc94d885020f7697bcc4d573ddddd263601e
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-18 15:14:24 -0500

    questions project updated

--- questions/app.js
+++ questions/app.js
@@ -1,11 +1,28 @@
-//using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle('show-text');
-    });
-});
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+//using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(qsn){
+    const btn = qsn.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        questions.forEach(function(item){
+            if (item != qsn){
+                item.classList.remove('show-text');
+            }
+        })
+
+        qsn.classList.toggle('show-text');
+    })
+})
+
