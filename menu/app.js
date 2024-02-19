151d4a0bda4b04c927548ee16b27f9dd3aeb360f
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-18 23:08:03 -0500

    The menu project is completed

--- menu/app.js
+++ menu/app.js
@@ -81,7 +81,29 @@ const container = document.querySelector('.btn-container');
 //load items
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
+  displayMenuButtons();
+});
 
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    return `<article class="menu-item">
+    <img src=${item.img} alt=${item.title} class="photo">
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  })
+  displayMenu = displayMenu.join("")
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons(){
   const categories = menu.reduce(function(values,item){
     if(!values.includes(item.category)){
       values.push(item.category);
@@ -112,24 +134,5 @@ window.addEventListener('DOMContentLoaded', function(){
       }
     })
   })
-});
-
-function displayMenuItems(menuItems){
-  let displayMenu = menuItems.map(function(item){
-    return `<article class="menu-item">
-    <img src=${item.img} alt=${item.title} class="photo">
-    <div class="item-info">
-      <header>
-        <h4>${item.title}</h4>
-        <h4 class="price">${item.price}</h4>
-      </header>
-      <p class="item-text">
-        ${item.desc}
-      </p>
-    </div>
-  </article>`;
-  })
-  displayMenu = displayMenu.join("")
-  sectionCenter.innerHTML = displayMenu;
 }
 
