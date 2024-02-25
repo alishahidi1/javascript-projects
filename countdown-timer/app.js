b43cce9d5d14d15aed51efee2e94d0077eec38dd
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-25 14:02:16 -0500

    countdown's JS is updated

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -26,8 +26,14 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2024, 4, 26, 19, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+// let futureDate = new Date(2024, 4, 26, 19, 30, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 7);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const mins = futureDate.getMinutes();
