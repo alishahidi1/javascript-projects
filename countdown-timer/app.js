95f8a3716ed99f5cee9909ef852645cb6e1d71fa
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-02-25 13:17:01 -0500

    countdown's JS initialized

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -21,3 +21,37 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const item = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2024, 4, 26, 19, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const day = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hours}:${mins}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  const oneDay = 24*60*60*1e3;
+  const oneHour = 3600*1e3;
+  const oneMin = 60*1e3;
+  const oneSec = 1e3;
+
+  let days = Math.floor(t/oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let mins = Math.floor((t % oneHour) / oneMin);
+  let secs = Math.floor((t % oneMin) / oneSec);
+}
